f852aeb55b43464987256ce1ee29926a499629ee
Pranav <ppranav164@gmail.com>
2023-07-22 21:38:31 +0530

    first commit

---
+++ app/Providers/NativeAppServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Providers;
+
+use Native\Laravel\Facades\ContextMenu;
+use Native\Laravel\Facades\Dock;
+use Native\Laravel\Facades\Window;
+use Native\Laravel\GlobalShortcut;
+use Native\Laravel\Menu\Menu;
+
+class NativeAppServiceProvider
+{
+    /**
+     * Executed once the native application has been booted.
+     * Use this method to open windows, register global shortcuts, etc.
+     */
+    public function boot(): void
+    {
+        Menu::new()
+            ->appMenu()
+            ->submenu('NativePHP', Menu::new()
+                ->link('https://nativephp.com', 'Documentation')
+            )
+            ->submenu('Volt', Menu::new()
+            ->link('https://nativephp.com', 'Documentation')
+            )
+            ->register();
+ 
+        Window::open()
+            ->width(800)
+            ->height(800);
+
+        /**
+            Dock::menu(
+                Menu::new()
+                    ->event(DockItemClicked::class, 'Settings')
+                    ->submenu('Help',
+                        Menu::new()
+                            ->event(DockItemClicked::class, 'About')
+                            ->event(DockItemClicked::class, 'Learn More…')
+                    )
+            );
+
+            ContextMenu::register(
+                Menu::new()
+                    ->event(ContextMenuClicked::class, 'Do something')
+            );
+
+            GlobalShortcut::new()
+                ->key('CmdOrCtrl+Shift+I')
+                ->event(ShortcutPressed::class)
+                ->register();
+        */
+    }
+}
